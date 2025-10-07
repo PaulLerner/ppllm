29f859504e61e5a339b077d48148f7328ba13266
Paul Lerner <lerner@tuta.io>
2025-10-07 08:54:55 +0200

    replace vllm by hf

--- ppllm/ppl.py
+++ ppllm/ppl.py
@@ -2,69 +2,107 @@ from jsonargparse import CLI
 import json
 from dataclasses import dataclass, asdict
 from pathlib import Path
-from typing import Optional, Any, Dict
+from typing import Optional, Any, Dict, Union
 from jsonargparse import CLI
+import os
+import warnings
+from tqdm import tqdm
 
-import numpy as np
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
 
-from vllm import LLM, SamplingParams
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 from .utils import load_texts
 
 
 @dataclass
-class LlmArguments:
-    model: str
-    tokenizer: Optional[str] = None
-    tokenizer_mode: str = "auto"
-    skip_tokenizer_init: bool = False
+class ModelKwargs:
+    pretrained_model_name_or_path: Optional[Union[str, os.PathLike]] = None
+    #device_map: str = "auto"
+    config: Optional[Union[str, os.PathLike]] = None
+    cache_dir: Optional[Union[str, os.PathLike]] = None
+    ignore_mismatched_sizes: bool = False
+    force_download: bool = False
+    local_files_only: bool = False
+    token: Optional[Union[str, bool]] = None
+    revision: str = "main"
+    use_safetensors: bool = None
+    resume_download: bool = False
+    output_loading_info: bool = False
+    dtype: str = "float16"
+    load_in_8bit: bool = False
+    load_in_4bit: bool = False
+   # use_flash_attention_2: bool = False
     trust_remote_code: bool = True
-    tensor_parallel_size: int = 1
-    dtype: str = "auto"
-    quantization: Optional[str] = None
-    revision: Optional[str] = None
-    tokenizer_revision: Optional[str] = None
-    seed: int = 0
-    gpu_memory_utilization: float = 0.9
-    swap_space: float = 4
-    cpu_offload_gb: float = 0
-    enforce_eager: Optional[bool] = False
-    max_seq_len_to_capture: int = 8192
-    disable_custom_all_reduce: bool = False
-    disable_async_output_proc: bool = False
-    mm_processor_kwargs: Optional[Dict[str, Any]] = None
-    max_model_len: int = None
 
 
-def main(output_dir: Path, data_path: Path, llm_arguments: LlmArguments, input_key: str = "text", split: str = "test"):
-    output_dir.mkdir(exist_ok=True, parents=True)
-    llm = LLM(**asdict(llm_arguments))
-    texts = load_texts(data_path, input_key=input_key, split=split)
-    sampling_params = SamplingParams(prompt_logprobs=1, max_tokens=1)
-    outputs = llm.generate(texts, sampling_params)
-    assert len(outputs) == len(texts)
-    total_logps, total_chars, total_tokens = [], [], []
-    for text, output in zip(texts, outputs):
-        assert output.prompt_logprobs[0] is None
-        total_logp = 0
-        for logps in output.prompt_logprobs[1:]:
-            assert len(logps) <= 2
-            logps = list(logps.values())
-            # vllm always include the most probable token as well, 
-            # we want to make sure it's at index 1 and that the attested token is at index 0
-            if len(logps) == 2:
-                assert logps[0].logprob <= logps[1].logprob
-            total_logp += logps[0].logprob
-        total_logps.append(total_logp)
-        total_tokens.append(len(output.prompt_logprobs[1:]))
+@dataclass
+class LoaderKwargs:
+    batch_size: Optional[int] = 64
+    num_workers: int = 4
+    pin_memory: bool = False
+    drop_last: bool = False
+    timeout: float = 0
+    prefetch_factor: Optional[int] = None
+    persistent_workers: bool = False
+    pin_memory_device: str = ""
+
+
+@dataclass
+class TokenizerKwargs:
+    return_tensors: str = 'pt'
+    padding: Union[bool, str] = 'longest'
+    truncation: bool = False
+    return_overflowing_tokens: bool = False
+    max_length: int = None
+
+
+@torch.no_grad()
+def compute_nll(loader, model, tokenizer, tokenizer_kwargs):
+    loss_fct = nn.CrossEntropyLoss(reduction="none", ignore_index=tokenizer.pad_token_id)
+    total_losses, total_chars, total_tokens = [], [], []
+    for batch in tqdm(loader, total=len(loader.dataset)//loader.batch_size):
+        inputs = tokenizer(batch, **tokenizer_kwargs)
+        for k, v in inputs.items():
+            inputs[k] = v.to(model.device)
+        logits = model(**inputs, return_dict=True).logits
+        batch_size, seq_len = inputs["input_ids"].shape
+        labels = inputs['input_ids']        
+        logits = logits[:, :-1].contiguous().view(-1, model.config.vocab_size)
+        labels = labels[:, 1:].contiguous().view(-1)
+        losses = loss_fct(logits, labels).view(batch_size, seq_len-1)
+        total_losses.append(losses.sum(1))
+        tokenized_texts = tokenizer(batch, add_special_tokens=False)
         # FIXME: if there's no BOS, we should not count the first token
-        total_chars.append(len(text))
-    total_logps, total_chars, total_tokens = np.array(total_logps), np.array(total_chars), np.array(total_tokens)
-    
+        for text, tokens in zip(batch, tokenized_texts["input_ids"]):
+            total_chars.append(len(text))
+            total_tokens.append(len(tokens))
+    total_losses = torch.cat(total_losses).to(torch.float32)
+    total_chars, total_tokens = torch.tensor(total_chars), torch.tensor(total_tokens)
+    return total_losses, total_chars, total_tokens
+
+
+def main(output_dir: Path, data_path: Path, model_kwargs: ModelKwargs, input_key: str = "text", split: str = "test",
+         tokenizer_kwargs: TokenizerKwargs = TokenizerKwargs(), loader_kwargs: LoaderKwargs = LoaderKwargs()):
+    output_dir.mkdir(exist_ok=True, parents=True)
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_kwargs.pretrained_model_name_or_path, 
+        add_prefix_space=False, 
+        add_eos_token=False, 
+        trust_remote_code=model_kwargs.trust_remote_code
+    )
+    if tokenizer.pad_token is None:
+        warnings.warn(f"{tokenizer.pad_token=}, setting to {tokenizer.eos_token=}")
+        tokenizer.pad_token = tokenizer.eos_token
+    model = AutoModelForCausalLM.from_pretrained(**asdict(model_kwargs)).cuda()
+    texts = load_texts(data_path, input_key=input_key, split=split)
+    loader = DataLoader(texts, **asdict(loader_kwargs), shuffle=False, collate_fn=None)
+    total_losses, total_chars, total_tokens = compute_nll(loader, model, tokenizer, asdict(tokenizer_kwargs))
     # surprisal is expressed in bits
-    total_surprisal = -total_logps.sum()/np.log(2)
+    total_surprisal = total_losses.sum()/torch.log(torch.tensor(2))
     metrics = {
-        # N.B. this is the same as `np.exp(total_logps.sum()/total_tokens.sum())`
         "ppl": 2**(total_surprisal/total_tokens.sum()).item(),
         "bpc": (total_surprisal/total_chars.sum()).item(),
         "surprisal": total_surprisal.item()
@@ -73,9 +111,8 @@ def main(output_dir: Path, data_path: Path, llm_arguments: LlmArguments, input_k
     print(metrics)
     with open(output_dir/"metrics.json", "wt") as file:
         json.dump(metrics, file)
-    for name, array in zip(["log_prob", "char", "tokens"], [total_logps, total_chars, total_tokens]):
-        with open(output_dir/f"{name}.npy", "wb") as file:
-            np.save(file, array)
+    for name, array in zip(["nll", "char", "tokens"], [total_losses, total_chars, total_tokens]):
+        torch.save(array, output_dir/f"{name}.bin")
 
 
 def cli():
